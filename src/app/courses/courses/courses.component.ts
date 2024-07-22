43dadaa03d4b3361d1677324f529d2aab1542581
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2024-07-22 11:45:36 -0300

    Courses List Spinner Loading...

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -2,28 +2,25 @@ import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 import { Observable } from 'rxjs';
-
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss'],
+  styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
   // courses: Course[] = [];
-
-  displayedColumns = ['name', 'category'];
+  displayedColumns = ['name','category'];
 
   // coursesService: CoursesService;
-
   constructor(private coursesService: CoursesService) {
     // this.courses = [];
     // this.coursesService = new CoursesService();
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list();
+
+    // this.coursesService.list().subscribe(courses => this.courses = courses);
   }
-
   ngOnInit(): void {
-
   }
 }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -2,22 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Course } from '../model/course';
-import { first, tap } from 'rxjs/operators';
+import { delay, first, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoursesService {
-
   private readonly API = '/assets/courses.json';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   list() {
-    return this.httpClient.get<Course[]>(this.API)
-    .pipe(
+    return this.httpClient.get<Course[]>(this.API).pipe(
       first(),
-      tap(courses => console.log(courses))
+      delay(5000),
+      tap((courses) => console.log(courses))
     );
   }
 }
