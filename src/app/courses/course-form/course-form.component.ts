8e08f06f14cb48a869883739362ad8bf89de35ff
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2024-07-31 10:15:56 -0300

    Saving Data with HTTP POST + errors treatment

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { CoursesService } from '../services/courses.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-course-form',
@@ -10,7 +12,9 @@ export class CourseFormComponent implements OnInit {
 
   form: FormGroup
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private _snackBar: MatSnackBar) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
@@ -22,11 +26,20 @@ export class CourseFormComponent implements OnInit {
   }
 
   onSubmit(){
-
+    this.service.save(this.form.value)
+      .subscribe({
+        next: result => console.log(result),
+        error: () => this.onError(),
+        complete: () => console.log('Save completed')
+      });
   }
 
   onCancel(){
 
+  }
+
+  private onError(){
+    this._snackBar.open('Erro ao salvar curso', '', {duration: 5000});
   }
 
 }
